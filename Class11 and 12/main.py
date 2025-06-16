e8ba5e72f4d0be836c24793d8fa5be8e52777eed
EDUARDO DA SILVA PAIM <85540781015@senacrs.edu.br>
2025-06-16 19:29:23 -0300

    update

---
+++ Class11 and 12/main.py
@@ -0,0 +1,24 @@
+from Fila import Fila
+
+fila = Fila()
+fila.imprimir()
+fila.add( "Victor")
+fila.add( "Butiá" )
+fila.add( "Lisiane" )
+
+fila.remover()
+fila.remover()
+fila.add( "Roberto" )
+fila.remover()
+fila.remover()
+
+# 1) implemente uma fila de carros em um Lava-Jato
+# contendo um método para registrar a entrada
+# dos carros e uma método para lavar os carros
+# O carro deve conter os atributos
+# modelo, ano e placa
+# desenvolva também um método para imprimir a 
+# fila de carros
+
+#2) Implemente um sistema de gestão de fila de um 
+#  banco que tem uma fila normal e uma prioritária
