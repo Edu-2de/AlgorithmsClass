68ba8419b7c8cbf847d6bcf39d5d8badae5a1af8
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-26 14:21:57 -0300

    Initialize guitars list in Color class constructor

--- Example4/Color.py
+++ Example4/Color.py
@@ -4,6 +4,7 @@ class Color:
       def __init__(self, name: str = None, hex_value: str = None):
             self.name = name
             self.hex_value = hex_value
+            self.guitars = []
       
       def __str__(self):
             return f"Color: {self.name}, Hex: {self.hex_value}"
