ae5ca406589846afdf700db1f5d401bfcca98538
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-26 14:08:58 -0300

    Add Guitarra class with initializer for brand, model, year, and price attributes

---
+++ Example4/Guitarra.py
@@ -0,0 +1,6 @@
+class Guitarra:
+      def __init__(self, brand:str = None, model: str = None, year:int = 0, price:float = 0.0):
+            self.brand = brand
+            self.model = model
+            self.year = year
+            self.price = price
