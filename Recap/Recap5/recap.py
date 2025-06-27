5495d6723b936ce50bf0037ad4fa9a6fe9a656ca
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-27 12:41:08 -0300

    Add Person class with initialization and print_name method

---
+++ Recap/Recap5/recap.py
@@ -0,0 +1,8 @@
+class Person:
+      def __init__(self, __code: int = 0, name: str = None, _address: str = None, __phone: str = None):
+            self.__code = __code
+            self.name = name
+            self._address = _address
+            self.__phone = __phone
+
+      def print_name(self) 
