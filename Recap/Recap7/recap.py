878d9413dee03e75670149fa0034390655618d1a
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-28 19:24:36 -0300

    Add ListaEncadeada class with methods to initialize and add nodes

--- Recap/Recap7/recap.py
+++ Recap/Recap7/recap.py
@@ -2,3 +2,17 @@ class No:
       def __init__(self, valor):
             self.valor = valor
             self.proximo = None
+
+class ListaEncadeada:
+      def __init__(self):
+            self.inicio = None
+
+      def adicionar(self, valor):
+            novo_no = No(valor)
+            if not self.inicio:
+                  self.inicio = novo_no
+            else:
+                  atual = self.inicio
+                  while atual.proximo:
+                        atual = atual.proximo
+                  atual.proximo = novo_no
