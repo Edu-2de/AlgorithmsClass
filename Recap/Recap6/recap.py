4a3145c35c638939f6ea7d5f889ceabc5b9d8b4d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-28 19:15:27 -0300

    Refactor display_values method in Current_account class to print account type

--- Recap/Recap6/recap.py
+++ Recap/Recap6/recap.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 
 class Bank_account(ABC):
-    @abstractmethod
-    def display_values(self):
-        pass
+      @abstractmethod
+      def display_values(self):
+            pass
     
 class Current_account(Bank_account):
       def display_values(self):
-        return f"Current Account {self.account_number}: Balance = ${self.balance:.2f}"
+            print("Current Account")
