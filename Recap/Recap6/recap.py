e7e31c3728978267720ffa1b2c03ec25c9f8acb8
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-28 19:14:50 -0300

    Add Current_account class implementing display_values method

--- Recap/Recap6/recap.py
+++ Recap/Recap6/recap.py
@@ -4,3 +4,7 @@ class Bank_account(ABC):
     @abstractmethod
     def display_values(self):
         pass
+    
+class Current_account(Bank_account):
+      def display_values(self):
+        return f"Current Account {self.account_number}: Balance = ${self.balance:.2f}"
