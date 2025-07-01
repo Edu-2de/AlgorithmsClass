525d6308e54045637f00d438f7b455821648abcd
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-01 14:07:09 -0300

    Add senha parameter to __init__ method and implement password verification in sacar method

--- Recap/Recap13/recap.py
+++ Recap/Recap13/recap.py
@@ -4,16 +4,23 @@ import string
 class Conta:
     _contador_id = 0
   
-    def __init__(self, _saldo:float = 0.0, titular:str = None):
+    def __init__(self, _saldo:float = 0.0, titular:str = None, senha:str = None):
         self.id = Conta._contador_id
         Conta._contador_id += 1
         self.__numero_conta =  ''.join(random.choices(string.ascii_letters + string.digits, k=5))
         self._saldo = _saldo
         self.titular = titular
+        self.senha = senha
 
     def __str__(self):
         return f"A conta: {self.__numero_conta} do titular: {self.titular} foi criada com sucesso"
     
-    def sacar(self, valor:float):
-        if valor <= self._saldo:
-            print(f"Voce sacou: {valor} da sua conta, agora voce tem: {self._saldo} de saldo restante")
+    def sacar(self, valor:float, senha:str):
+        if senha == self.senha:
+            if valor <= self._saldo:
+                self._saldo -= valor
+                print(f"Voce sacou: {valor} da sua conta, agora voce tem: {self._saldo} de saldo restante")
+            else:
+                print(f"Voce nao tem saldo suficiente para sacar essa quantia")
+        else:
+            print(f"Voce digitou a senha errada, tente novamente")
