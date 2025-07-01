dc0558ed5f9fbed554959d2ddac3f604644a5406
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-01 12:08:37 -0300

    Implement class Veiculo with constructor and __str__ method for vehicle representation

--- Recap/Recap12/recap.py
+++ Recap/Recap12/recap.py
@@ -12,3 +12,12 @@ Requisitos:
 - Implemente métodos assessores (getter) e modificadores (setter) para ambos os atributos privados.
 - Crie um arquivo main para testar a criação de um carro e de uma bicicleta, mostrando o funcionamento dos métodos e dos getters/setters.
 '''
+
+class Veiculo():
+    def __init__(self, id:int = 0, marca:str = None, modelo:str = None):
+        self.id = id + 1,
+        self.marca = marca, 
+        self.modelo = modelo
+
+    def __str__(self):
+        return f"Veiculo da marca: {self.marca} e do modelo: {self.modelo} foi criado com sucesso"
