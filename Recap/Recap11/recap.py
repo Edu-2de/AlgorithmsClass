4848f3e0a33ae2944e6fc2eecb6038dc11d7ea65
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-01 11:34:09 -0300

    Add class diagram requirements and abstract class specifications to recap.py

---
+++ Recap/Recap11/recap.py
@@ -0,0 +1,12 @@
+'''
+Construa um algoritmo para implementar o seguinte diagrama de classes:
+Requisitos:
+
+- A classe pessoa é abstrata
+- O método marcarPresenca da classe pessoa é abstrato
+- O atributo cpf é fracamente privado
+- O atributo matrícula é fortemente privado
+
+Construa um método assessor e um método modificador para o atributo matrícula
+Construa um arquivo main para testar a construção de um aluno
+'''
