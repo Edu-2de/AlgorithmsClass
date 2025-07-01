146f5b3ded9ecf1f9be52dd5bfa8c7bbb5b25067
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-01 14:02:02 -0300

    Fix __init__ method in Conta class to remove unused __numero_conta parameter and ensure account number is generated automatically

--- Recap/Recap13/recap.py
+++ Recap/Recap13/recap.py
@@ -1,9 +1,15 @@
+import random
+import string
+
 class Conta:
     _contador_id = 0
-    def __init__(self, _saldo:float = 0.0, __numero_conta:str = None, titular:str = None)
+  
+    def __init__(self, _saldo:float = 0.0, titular:str = None):
         self.id = Conta._contador_id
         Conta._contador_id += 1
-        self.__numero_conta = __numero_conta
+        self.__numero_conta =  ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+        self._saldo = _saldo
         self.titular = titular
 
-    def
+    def __str__(self):
+        return f"A conta {self.__numero_conta} do titular: {self.titular} foi criada com sucesso"
