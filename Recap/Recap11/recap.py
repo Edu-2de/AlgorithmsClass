fa6a1ba5cb511a7a0e9fb98bc7e0e51f1dd77a0d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-01 12:04:35 -0300

    Add methods to Pessoa class for attendance marking and enrollment processing

--- Recap/Recap11/recap.py
+++ Recap/Recap11/recap.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 '''
 Construa um algoritmo para implementar o seguinte diagrama de classes:
 Requisitos:
@@ -20,9 +23,36 @@ class Pessoa:
     def __str__(self):
         return f"Pessoa do id: {self.id}, nome: {self.nome} e de cpf: {self._cpf} foi cadastrada com sucesso"
     
+    def marcarPresenca(self) -> None:
+        print(f"{self.nome} esta presente")
+
+    def matricular(self) -> None:
+        print(f"Matricula sendo realizada se a pessoa for um aluno")
+
+    
 class Aluno(Pessoa):
     def __init__(self, id:int = 0, nome:str = None, _cpf:str = None, __matricula:str = None):
-        
+        super().__init__(id, nome, _cpf),
+        self.__matricula = __matricula
+
+    def __str__(self):
+        return f"Aluno do id: {self.id}, nome: {self.nome}, cpf: {self._cpf} e matricula: {self.__matricula} foi adicionado(a) com sucesso"
+    
+    def marcarPresenca(self) -> None:
+        super().marcarPresenca()
+
+    
+    def  matricular(self) -> str:
+        super().matricular()
+        tamanho = 8
+        aleatorio = ''.join(random.choices(string.ascii_letters + string.digits, k=tamanho))
+        self.__matricula = aleatorio
+        return f"Aluno {self.nome} cadastrado com sucesso de matricula: {self.__matricula}"
+    
+    
+    
+
+      
 
 
 
