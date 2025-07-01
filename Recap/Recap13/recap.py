de0f35cfa206714b06c80a9f7771c955f5b08cc9
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-01 13:21:42 -0300

    Fix __init__ method in Conta class to include parameters for saldo, numero_conta, and titular

--- Recap/Recap13/recap.py
+++ Recap/Recap13/recap.py
@@ -1,5 +1,9 @@
 class Conta:
     _contador_id = 0
-    def __init__(self, )
+    def __init__(self, _saldo:float = 0.0, __numero_conta:str = None, titular:str = None)
         self.id = Conta._contador_id
         Conta._contador_id += 1
+        self.__numero_conta = __numero_conta
+        self.titular = titular
+
+    def
