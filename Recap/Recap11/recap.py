6ecfc1eb08091e46037aec513d3b5774e3c7a3b9
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-01 11:42:44 -0300

    Refactor Pessoa class constructor and __str__ method for improved attribute handling

--- Recap/Recap11/recap.py
+++ Recap/Recap11/recap.py
@@ -12,7 +12,14 @@ Construa um arquivo main para testar a construção de um aluno
 '''
 
 class Pessoa:
-      def __init__(self, id:int = 0, nome:str = None, cpf:str = None):
-            self.id = id + 1,
+    def __init__(self, id:int = 0, nome:str = None, cpf:str = None):
+        self.id = id + 1,
+        self.nome = nome,
+        self.cpf = cpf
+
+    def __str__(self):
+        return print(f"Pessoa do id: {self.id}, nome: {self.nome} e cpf: {self.cpf} foi cadastrada com sucesso")
+
+
 
  
