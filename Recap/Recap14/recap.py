3ae646333cf9b1afb79c2b8dfb2625f4d58ba5e8
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-02 21:51:21 -0300

    Fix syntax error in Livro class constructor and ensure proper attribute initialization

--- Recap/Recap14/recap.py
+++ Recap/Recap14/recap.py
@@ -1,6 +1,13 @@
+import random
+import string
+
 
 class Livro:
     _contador_id = 0
-    def __init__(self, titulo:str = None, autor:str = None, ano:int = 0000)
+    def __init__(self, titulo:str = None, autor:str = None, ano:int = 0000):
         self.id = Livro._contador_id
         Livro._contador_id += 1
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.codigo =  ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)) 
