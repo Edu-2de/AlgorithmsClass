3e0bdc2ff48dfe2342548a9df889b5f45aac6827
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-02 21:49:56 -0300

    Fix indentation and add missing newline in Livro class constructor

--- Recap/Recap14/recap.py
+++ Recap/Recap14/recap.py
@@ -1,3 +1,6 @@
 
 class Livro:
+    _contador_id = 0
     def __init__(self, titulo:str = None, autor:str = None, ano:int = 0000)
+        self.id = Livro._contador_id
+        Livro._contador_id += 1
