8911b34b19132e5a9d78e4f2ebbba386d8c109cf
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-07 19:39:33 -0300

    Refactor removerPelaSenha method to correctly handle removal of a person by senha, including updates for single and multiple elements in the queue.

--- Recap/Recap15/main.py
+++ Recap/Recap15/main.py
@@ -116,19 +116,18 @@ class ListaPessoa:
         if self.inicio:
             aux = self.inicio
             aux1 = self.inicio
-            while aux1 and aux1.proximo.pessoa.senha != senha_digitada):
-                aux1 = aux1.proximo
-            anterior = aux1
                 
             while(aux and aux.pessoa.senha != senha_digitada):
                 aux = aux.proximo
+                aux1 = aux1.proximo
             atual = aux
+            anterior = aux1
 
-            if self.tamanho == 1 and aux != None:
+            if aux == self.inicio:
                 self.inicio = self.inicio.proximo
                 self.tamanho -= 1
                 print(f"Pessoa: {atual.pessoa} foi removido(a) com sucesso!")
-                
+
             elif self.tamanho > 1 and aux != None:
                 anterior.proximo = atual.proximo
                 self.tamanho -= 1
