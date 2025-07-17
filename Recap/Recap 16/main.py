87e87e0c1fd1f1918786831739f8f4be7ce1aa0d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-17 14:02:02 -0300

    Fix Music class string representation method and ensure proper formatting

--- Recap/Recap 16/main.py
+++ Recap/Recap 16/main.py
@@ -8,4 +8,5 @@ class Music:
     self.duration = duration,
     self.code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
 
-  
+  def __str__(self):
+    return f'Music{self.title}, from artist: {self.artist}, duration: {self.duration}'
