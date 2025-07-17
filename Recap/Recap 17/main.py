24d01eb1989fb3dad663cacd0a01a951bd1413b2
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-17 18:33:23 -0300

    Add CharacterNo class to manage linked character nodes

--- Recap/Recap 17/main.py
+++ Recap/Recap 17/main.py
@@ -4,3 +4,10 @@ class Character:
 
   def __str__(self):
     return f"Character: {self.value}"
+  
+class CharacterNo:
+  def __init__(self, character):
+    self.character = character
+    self.next = None
+    self.previous = None
+    self.cursor = None
