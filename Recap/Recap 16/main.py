3e1917b72d0aec3bcac0e9fa87cc6a6bdf356d27
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-17 14:06:01 -0300

    Fix Music class constructor by removing trailing commas and ensure proper string representation formatting

--- Recap/Recap 16/main.py
+++ Recap/Recap 16/main.py
@@ -3,10 +3,22 @@ import string
 
 class Music:
   def __init__(self, title:str = None, artist:str = None, duration:int = 0):
-    self.title = title,
-    self.artist = artist,
-    self.duration = duration,
+    self.title = title
+    self.artist = artist
+    self.duration = duration
     self.code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
 
   def __str__(self):
     return f'Music{self.title}, from artist: {self.artist}, duration: {self.duration}'
+  
+class NoMusic:
+    def __init__(self, music):
+      self.music = music
+      self.proximo = None
+      self.anterior = None
+
+class MusicList:
+  def __init__(self):
+    self.start = None
+    self.length = 0
+  
