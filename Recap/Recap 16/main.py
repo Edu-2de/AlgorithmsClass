52876664a310e258cbfd9ddb8dd327622d529679
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-17 14:00:58 -0300

    Refactor Music class constructor to include type hints and default values for parameters

--- Recap/Recap 16/main.py
+++ Recap/Recap 16/main.py
@@ -2,8 +2,10 @@ import random
 import string
 
 class Music:
-  def __init__(self, title, artist, duration, ):
+  def __init__(self, title:str = None, artist:str = None, duration:int = 0):
     self.title = title,
     self.artist = artist,
     self.duration = duration,
     self.code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+
+  
