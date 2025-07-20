d32fdca6ed1ebb3b7601e3ac3399a16cb57d06c4
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-20 12:56:14 -0300

    Add BookPhysical and BookList classes with initializers

--- Recap/Recap 20/main.py
+++ Recap/Recap 20/main.py
@@ -8,9 +8,18 @@ class Book():
   def __str__(self):
     return f"{self.title} - {self.author} - {self.year} - {self.available}"
   
+class BookPhysical():
+  def __init__(self, title:str = None, author:str = None, year:int = 0000):
+    super().__init__(title, author, year)
+    
+  
 class BookNo():
   def __init__(self, book):
     self.book = book
     self.next = None
     self.previous = None
  
+class BookList():
+  def __init__(self):
+    self.start = None
+    self.length = 0
