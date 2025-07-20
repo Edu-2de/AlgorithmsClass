a56f1e5d92dcd3329b61051b371697fd0cc56f80
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-20 12:52:22 -0300

    Fix Book class initializer by adding missing colon and ensuring proper attribute assignment

--- Recap/Recap 20/main.py
+++ Recap/Recap 20/main.py
@@ -1,2 +1,9 @@
 class Book():
-  def __init__(self, title:str = None, author:str = None, year:int = 0000)
+  def __init__(self, title:str = None, author:str = None, year:int = 0000):
+    self.title = title
+    self.author = author
+    self.year = year
+    self.available = True
+
+  
+ 
